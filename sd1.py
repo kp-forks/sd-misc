985d6e9be834a1be5322d5e2301905518b6f5830
tangtang30 <145232910+tangtang30@users.noreply.github.com>
2023-09-17 07:44:27 +0800

    Update sd1.py

--- sd1.py
+++ sd1.py
@@ -5,6 +5,8 @@ import subprocess
 import shutil
 from IPython.display import clear_output
 
+from google.colab import drive
+drive.mount('/content/drive')
 
 %cd /content/
 !git clone -b master --single-branch https://github.com/AUTOMATIC1111/stable-diffusion-webui /content/colab
