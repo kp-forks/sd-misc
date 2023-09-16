24aeec1a3ca6f81d9958fa87adeb82b3f3a05e5d
tangtang30 <145232910+tangtang30@users.noreply.github.com>
2023-09-17 06:48:42 +0800

    Update sd1.py

--- sd1.py
+++ sd1.py
@@ -1,4 +1,3 @@
-#@title ## 1、加载谷歌云盘并初始化环境
 import os
 import concurrent.futures
 import time
@@ -15,10 +14,7 @@ sd=f"{sd0}{sd1}-{wi}"
 sdir="drive/MyDrive/sd"
 sddir='colab'
 
-apt-get -y install -qq aria2
-if os.path.exists(f'/content/{sddir}'):
-  shutil.rmtree(f'/content/{sddir}')
-!mkdir /content/models
+
 
 %cd /content/
 !git clone -b master --single-branch https://github.com/AUTOMATIC1111/{sd} /content/{sddir}
