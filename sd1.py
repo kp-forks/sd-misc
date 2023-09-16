417fe0c615adbc33822fa2f985708bfa01e2bf73
tangtang30 <145232910+tangtang30@users.noreply.github.com>
2023-09-17 07:44:00 +0800

    Update sd1.py

--- sd1.py
+++ sd1.py
@@ -6,18 +6,8 @@ import shutil
 from IPython.display import clear_output
 
 
-
-sd0="sta"+"ble-di"
-sd1="ffu"+"sion"
-wi="w"+"ebui"
-sd=f"{sd0}{sd1}-{wi}"
-sdir="drive/MyDrive/sd"
-sddir='colab'
-
-
-
 %cd /content/
-!git clone -b master --single-branch https://github.com/AUTOMATIC1111/{sd} /content/{sddir}
+!git clone -b master --single-branch https://github.com/AUTOMATIC1111/stable-diffusion-webui /content/colab
 
-
+clear_output()
 
