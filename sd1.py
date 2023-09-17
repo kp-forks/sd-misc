efc67d0e7a10d1593ab3831939e63813cf7db97e
tangtang30 <145232910+tangtang30@users.noreply.github.com>
2023-09-17 08:06:05 +0800

    Update sd1.py

--- sd1.py
+++ sd1.py
@@ -8,7 +8,7 @@ from IPython.display import clear_output
 from google.colab import drive
 drive.mount('/content/drive')
 
-cd /content/
+
 git clone -b master --single-branch https://github.com/AUTOMATIC1111/stable-diffusion-webui /content/colab
 
 clear_output()
