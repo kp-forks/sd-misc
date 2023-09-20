bb311978a7a6df7444ba599750dcde110b9593be
tangtang30 <145232910+tangtang30@users.noreply.github.com>
2023-09-20 19:12:59 +0800

    Update tlsd.py

--- tlsd.py
+++ tlsd.py
@@ -33,8 +33,8 @@ def task1():
  subprocess.run(f'git clone https://huggingface.co/nolanaatama/embeddings {params["sd_dir"]}/embeddings',shell=True)
  
  end_time=time.time()
- print("\ngit spent:",end_time-start_time,"s")
-def run_aria2c_download():
+ print("\ntask1 spent:",end_time-start_time,"s")
+def task2():
  start_time=time.time()
  subprocess.run(f'aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://huggingface.co/lokCX/4x-Ultrasharp/resolve/main/4x-UltraSharp.pth -d {params["sd_dir"]}/models/ESRGAN/ -o 4x-UltraSharp.pth',shell=True)
  subprocess.run(f'aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://huggingface.co/datasets/daasd/CN.csv/resolve/main/CN.csv -d {params["sd_dir"]}/extensions/a1111-sd-webui-tagcomplete/tags -o CN.csv',shell=True) 
@@ -46,37 +46,37 @@ def run_aria2c_download():
   else:
    print('default')
  end_time=time.time() 
- print("\naria2c spent:",end_time-start_time,"s")
+ print("\ntask2 spent:",end_time-start_time,"s")
  
-def curl_download(): 
+def task3(): 
  start_time=time.time()
  subprocess.run(f"curl -Lo '{params['sd_dir']}/models/VAE/vae-ft-mse-840000-ema-pruned.safetensors' https://huggingface.co/stabilityai/sd-vae-ft-mse-original/resolve/main/vae-ft-mse-840000-ema-pruned.safetensors",shell=True)
  #subprocess.run(f"curl -Lo '{params['sd_dir']}/models/VAE/kl-f8-anime2.ckpt' https://huggingface.co/hakurei/waifu-diffusion-v1-4/resolve/4c4f05104055c029ad577c18ac176462f0d1d7c1/vae/kl-f8-anime2.ckpt",shell=True)
  #subprocess.run(f"curl -Lo '{params['sd_dir']}/models/VAE/animevae.pt' https://huggingface.co/swl-models/animvae/resolve/main/animevae.pt",shell=True)
  end_time=time.time()
- print("\ncurl spent:",end_time-start_time,"s")
-def wget_download():
+ print("\ntask3 spent:",end_time-start_time,"s")
+def task4():
  start_time=time.time()
  subprocess.run("apt install libunwind8-dev -yqq",shell=True) 
  os.environ["LD_PRELOAD"]="libtcmalloc.so.4"
  os.environ["TF_CPP_MIN_LOG_LEVEL"]="3"
  subprocess.run("sudo apt-get install sox ffmpeg libcairo2 libcairo2-dev",shell=True)
  end_time=time.time()
- print("\nwget spent:",end_time-start_time,"s")
-def pip_download():
+ print("\ntask4 spent:",end_time-start_time,"s")
+def task5():
  start_time=time.time() 
  #subprocess.run("pip install xformers xformers==0.0.20",shell=True)
  subprocess.run("pip install -q xformers==0.0.20 triton==2.0.0 -U",shell=True) 
  end_time=time.time()
- print("\npip spent:",end_time-start_time,"s")
+ print("\ntask5 spent:",end_time-start_time,"s")
 
 executor=concurrent.futures.ThreadPoolExecutor(max_workers=5)
 task1=executor.submit(task1)
-task2=executor.submit(run_aria2c_download)
-task3=executor.submit(curl_download)
-task4=executor.submit(wget_download)
-task5=executor.submit(pip_download)
-concurrent.futures.wait([task1,task2,task3,task4,task5])
+task2=executor.submit(task2)
+task3=executor.submit(task3)
+task4=executor.submit(task4)
+task5=executor.submit(task5)
+concurrent.futures.wait([task5,task4,task3,task2,task1])
 
 css_content = '''
   @media screen and (max-width: 600px) {
