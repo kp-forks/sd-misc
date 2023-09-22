1edc0332372510aa7ac3ef0a5beb9ffc0707b0c7
tangtang30 <145232910+tangtang30@users.noreply.github.com>
2023-09-22 15:12:20 +0800

    Update tlsd.py

--- tlsd.py
+++ tlsd.py
@@ -28,7 +28,7 @@ def task1():
  subprocess.run(f'git clone https://github.com/adieyal/sd-dynamic-prompts {params["sd_dir"]}/extensions/sd-dynamic-prompts',shell=True)
  #subprocess.run(f'git clone https://github.com/zanllp/sd-webui-infinite-image-browsing {params["sd_dir"]}/extensions/sd-webui-infinite-image-browsing',shell=True)
  subprocess.run(f'git clone https://github.com/BlafKing/sd-civitai-browser-plus {params["sd_dir"]}/extensions/sd-civitai-browser-plus',shell=True)
- #subprocess.run(f'git clone https://github.com/camenduru/tunnels {params["sd_dir"]}/extensions/tunnels',shell=True)
+ subprocess.run(f'git clone https://github.com/camenduru/tunnels {params["sd_dir"]}/extensions/tunnels',shell=True)
 
  if os.path.exists(f'{params["sd_dir"]}/embeddings'):
   shutil.rmtree(f'{params["sd_dir"]}/embeddings')
@@ -94,7 +94,7 @@ with open(f'{params["sd_dir"]}/style.css', 'a') as cssFile:
       cssFile.write(css_content)
 
 
-full_precision_str = params['user_arguments'] + " --disable-safe-unpickle --xformers --enable-insecure-extension-access --opt-sub-quad-attention --opt-channelslast --api --port 60088"
+full_precision_str = params['user_arguments'] + " --multiple --disable-safe-unpickle --xformers --enable-insecure-extension-access --opt-sub-quad-attention --opt-channelslast --api --port 60088"
 #full_precision_str=" --lowram --disable-safe-unpickle  --disable-console-progressbars --xformers --enable-insecure-extension-access --precision full --no-half --no-half-vae --opt-sub-quad-attention --opt-channelslast --api"
 
 full_precision_str+=" --theme='dark'"
