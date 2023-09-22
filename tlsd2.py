c3ec7822d62ff41acccdc41871745cf08a3135b8
tangtang30 <145232910+tangtang30@users.noreply.github.com>
2023-09-22 14:32:38 +0800

    Update tlsd2.py

--- tlsd2.py
+++ tlsd2.py
@@ -16,3 +16,8 @@ for arg in sys.argv[1:]:
 
 subprocess.run(f'git clone -b master --single-branch https://github.com/AUTOMATIC1111/stable-diffusion-webui {params["sd_dir"]}',shell=True)
 
+subprocess.run("apt install libunwind8-dev -yqq",shell=True) 
+os.environ["LD_PRELOAD"]="libtcmalloc.so.4"
+os.environ["TF_CPP_MIN_LOG_LEVEL"]="3"
+subprocess.run("apt install sox ffmpeg libcairo2 libcairo2-dev",shell=True)
+subprocess.run("pip install -q xformers==0.0.20 triton==2.0.0 -U",shell=True) 
