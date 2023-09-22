23df59ac7dd7aecf223a50f918807e02068387ff
tangtang30 <145232910+tangtang30@users.noreply.github.com>
2023-09-22 13:56:51 +0800

    Update tlsd2.py

--- tlsd2.py
+++ tlsd2.py
@@ -1,1 +1,18 @@
+import os
+import concurrent.futures
+import sys
+import time
+import subprocess
+import shutil
+from IPython.display import clear_output
+
+params={}
+for arg in sys.argv[1:]:
+ if arg.startswith('--'):
+  key_value=arg[len('--'):].split('=')
+  if len(key_value)==2:
+   key,value=key_value
+   params[key]=value
+
+subprocess.run(f'git clone -b master --single-branch https://github.com/AUTOMATIC1111/stable-diffusion-webui {params["sd_dir"]}',shell=True)
 
